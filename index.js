62b195e4495a40f42371dda06c1d89ff9594d48d
hexxa <hexxa@outlook.com>
2018-06-16 00:21:41 +0800

    fix: 1. read package.json 2.allow autoBump is not defined 3.fix unit tests

--- index.js
+++ index.js
@@ -3,19 +3,25 @@
 const { execSync } = require("child_process");
 const gitlog = require("gitlog");
 const { resolve } = require("path");
-const { writeFileSync } = require("fs");
+const fs = require("fs");
 const { AutoBump } = require("./auto_bump");
 const { FsUtil } = require("./fs_util");
 const { GitUtil } = require("./git_util");
+const { printError } = require("./print_util");
 
 function main() {
   const repoPath = resolve("./");
-  const config = require(`${repoPath}/package.json`);
-  const gitUtil = new GitUtil(execSync);
-  const fsUtil = new FsUtil(writeFileSync);
-  const bumper = new AutoBump(repoPath, config, gitlog, gitUtil, fsUtil);
+  try {
+    const config = JSON.parse(fs.readFileSync(`${repoPath}/package.json`));
+    const gitUtil = new GitUtil(execSync);
+    const fsUtil = new FsUtil(fs.writeFileSync);
+    const bumper = new AutoBump(repoPath, config, gitlog, gitUtil, fsUtil);
 
-  bumper.bump();
+    bumper.bump();
+  } catch (err) {
+    printError(err);
+    return;
+  }
 }
 
 main();
